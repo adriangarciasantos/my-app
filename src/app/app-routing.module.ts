47f1fc910c752dd567e1325ce1e685a9af10ce88
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2018-11-27 13:59:09 +0100

    Rutas 404, home etc...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FrutaComponent } from './fruta/fruta.component';
+import { FlujoInformacionComponent } from './flujo-informacion/flujo-informacion.component';
+import { ListadoClaseComponent } from './listado-clase/listado-clase.component';
+import { HomeComponent } from './components/home/home.component';
+import { Page404Component } from './components/page404/page404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'fruta', component: FrutaComponent},
+  {path: 'lista', component: ListadoClaseComponent},
+  {path: 'flujo', component: FlujoInformacionComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', component: Page404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
